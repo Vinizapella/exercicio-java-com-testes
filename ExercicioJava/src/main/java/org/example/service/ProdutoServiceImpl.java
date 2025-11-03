7684d62e338e35357a55ea931838834046637b6e
profviniciustrindade <vinicius.trindade@edu.sc.senai.br>
2025-11-03 14:23:59 -0300

    Ajuste projeto para facilitar exercicios dos alunos.

---
+++ ExercicioJava/src/main/java/org/example/service/ProdutoServiceImpl.java
@@ -0,0 +1,32 @@
+package org.example.service;
+
+import org.example.model.Produto;
+
+import java.util.List;
+
+public class ProdutoServiceImpl implements ProdutoService{
+    @Override
+    public Produto cadastrarProduto(Produto produto) {
+        return null;
+    }
+
+    @Override
+    public List<Produto> listarProdutos() {
+        return null;
+    }
+
+    @Override
+    public Produto buscarPorId(int id) {
+        return null;
+    }
+
+    @Override
+    public Produto atualizarProduto(Produto produto, int id) {
+        return null;
+    }
+
+    @Override
+    public boolean excluirProduto(int id) {
+        return false;
+    }
+}
